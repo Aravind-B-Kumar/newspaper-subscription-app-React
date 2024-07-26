2709d6abca7556806f65a03903679194bf303d4d
Aravind-B-Kumar <aravindkarullil@gmail.com>
2024-07-26 23:32:31 +0530

    view subscribers created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import AddSub from './components/AddSub';
+import ViewSub from './components/ViewSub';
 
 function App() {
   return (
     <div>
       
-      <AddSub/>
+      <ViewSub/>
       
 
     </div>
---
+++ src/components/ViewSub.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const ViewSub = () => {
+    return (
+        <div>
+
+            <NavBar />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <table class="table">
+
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Name</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+    
+                        </tr>
+
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">4</th>
+                            <td>Margaret</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">5</th>
+                            <td>Alexandra</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">6</th>
+                            <td>Ryan</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">7</th>
+                            <td>Harry</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">8</th>
+                            <td>Austin</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">9</th>
+                            <td>Peter</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">10</th>
+                            <td>Elizabeth</td>
+                        </tr>
+
+
+
+
+
+                    </tbody>
+                </table>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ViewSub
